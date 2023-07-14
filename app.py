bf92b07861d85d71d18f0cdb1edc3f377e2aa815
Siddhi Agarwal <siddhiag@iitbhilai.ac.in>
2023-07-14 17:47:18 +0530

    Added api files

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, send_file
+from pptx import Presentation
+import collections.abc
+
+app = Flask(__name__)
+
+# Create a presentation object
+presentation = Presentation()
+
+# Add a slide with a title and content
+slide_layout = presentation.slide_layouts[
+  1]  # Choose a slide layout (e.g., Title and Content)
+slide = presentation.slides.add_slide(slide_layout)
+
+title = slide.shapes.title
+title.text = "My First Slide"
+
+content = slide.placeholders[1]
+content.text = "Hello, PowerPoint!"
+
+# Save the presentation to a file
+presentation.save("my_presentation.pptx")
+
+
+@app.route('/')
+def index():
+  file_path = 'my_presentation.pptx'
+
+  return send_file(file_path, as_attachment=True)
+
+
+app.run(host='0.0.0.0', port=5000, debug=True)
