703d90034eb8453c4607db8caccb5cdceda89da2
SIDDHI AGARWAL <siddhiag@iitbhilai.ac.in>
2023-07-25 15:25:46 +0530

    Update app.py, made changes to makePPT func

--- app.py
+++ app.py
@@ -24,35 +24,34 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 # # Save the presentation to a file
 # presentation.save("my_presentation.pptx")
 
-def makePPT(data):
-  def _add_leveled_bullet(_placeholder, _text, level=0):
-    _prg = _placeholder.text_frame.add_paragraph()
-    _prg.level = level
-    _prg.text = _text
-  
-  # Create a presentation object
-  prs = Presentation()
-  title_slide_layout = prs.slide_layouts[0]
-  slide = prs.slides.add_slide(title_slide_layout)
-  title = slide.shapes.title
-  title.text = data['presentationTitle']
-  
-  for i in range(len(data['slide'])):
-    slide_layout = prs.slide_layouts[1]
-    slide = prs.slides.add_slide(slide_layout)
-    title = slide.shapes.title
-    subtitle = slide.placeholders[1]
-    title.text = data["slide"][i]["title"]
-    for j in range(len(data["slide"][i]["points"])):
-      _add_leveled_bullet(subtitle,data["slide"][i]["points"][j] , 0)
-  # Save the presentation to a file
-  prs.save("myPpresentation.pptx")
-
-
-
 @app.route('/',methods=['POST'])
 @cross_origin()
 def make():
+  def makePPT(data):
+    def _add_leveled_bullet(_placeholder, _text, level=0):
+      _prg = _placeholder.text_frame.add_paragraph()
+      _prg.level = level
+      _prg.text = _text
+    
+    # Create a presentation object
+    prs = Presentation()
+    title_slide_layout = prs.slide_layouts[0]
+    slide = prs.slides.add_slide(title_slide_layout)
+    title = slide.shapes.title
+    title.text = data['presentationTitle']
+    
+    for i in range(len(data['slide'])):
+      slide_layout = prs.slide_layouts[1]
+      slide = prs.slides.add_slide(slide_layout)
+      title = slide.shapes.title
+      subtitle = slide.placeholders[1]
+      title.text = data["slide"][i]["title"]
+      for j in range(len(data["slide"][i]["points"])):
+        _add_leveled_bullet(subtitle,data["slide"][i]["points"][j] , 0)
+    # Save the presentation to a file
+    prs.save("myPpresentation.pptx")
+
+  
   json_data = request.get_json()
   makePPT(json_data)
   file_path = 'my_presentation.pptx'
